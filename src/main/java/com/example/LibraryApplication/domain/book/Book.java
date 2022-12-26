df379280986f9d23082c3db7679c606c7fc06cff
henrieessalu <henri.eessalu@gmail.com>
2022-12-26 12:55:56 +0200

    Made addBook method

--- src/main/java/com/example/LibraryApplication/domain/book/Book.java
+++ src/main/java/com/example/LibraryApplication/domain/book/Book.java
@@ -15,29 +15,19 @@ public class Book {
     @Column(name = "id", nullable = false)
     private Integer id;
 
-    @Size(max = 255)
-    @NotNull
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Size(max = 255)
-    @NotNull
     @Column(name = "author", nullable = false)
     private String author;
 
-    @NotNull
     @Column(name = "release_year", nullable = false)
     private Integer releaseYear;
 
-    @NotNull
     @Column(name = "quantity", nullable = false)
     private Integer quantity;
 
-    @Size(max = 255)
-    @NotNull
     @Column(name = "location", nullable = false)
     private String location;
-
-
 
 }
